c3f7dbe6fa966fb44b7c41233096a723018ca9b5
zhangtao25 <tzhangm@trip.com>
2022-05-17 21:10:35 +0800

    添加redoc

--- copy-static-page.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs')
-fs.copyFile('./api-reference.html','./build/api-reference.html',function(err){
-    if(err) console.log('something wrong was happened')
-    else console.log('copy file succeed');
-})
